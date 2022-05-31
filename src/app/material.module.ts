71a6482618d16a7f1a8a0bca1b0f14f093952e9b
trevin <60538411+trevin-livele@users.noreply.github.com>
2022-05-31 14:44:53 +0300

    added date picker

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,14 +1,30 @@
-import { NgModule } from "@angular/core";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { MatButtonModule } from "@angular/material/button";
-import {MatIconModule} from '@angular/material/icon';
+import { NgModule } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 
 @NgModule({
-    imports :  [MatButtonModule,MatFormFieldModule,MatInputModule,MatIconModule,FlexLayoutModule ],
-    exports : [MatButtonModule,MatFormFieldModule,MatInputModule,MatIconModule,FlexLayoutModule ]
+  imports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    FlexLayoutModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
+  exports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    FlexLayoutModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+  ],
 })
-export class MaterialModule {
-
-}
+export class MaterialModule {}
