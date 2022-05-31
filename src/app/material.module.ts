590cf4e0c351bc89af9e7e8d43401144890d23cb
trevin <60538411+trevin-livele@users.noreply.github.com>
2022-05-31 14:03:57 +0300

    added flex layout

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,10 +3,11 @@ import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
 import {MatIconModule} from '@angular/material/icon';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
-    imports :  [MatButtonModule,MatFormFieldModule,MatInputModule,MatIconModule ],
-    exports : [MatButtonModule,MatFormFieldModule,MatInputModule,MatIconModule ]
+    imports :  [MatButtonModule,MatFormFieldModule,MatInputModule,MatIconModule,FlexLayoutModule ],
+    exports : [MatButtonModule,MatFormFieldModule,MatInputModule,MatIconModule,FlexLayoutModule ]
 })
 export class MaterialModule {
 
