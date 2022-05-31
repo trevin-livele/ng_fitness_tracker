403487771d5b38a2e9b118a93500daba7838047f
trevin <60538411+trevin-livele@users.noreply.github.com>
2022-05-31 13:47:21 +0300

    added the inputs

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from "@angular/core";
-import { MatButtonModule } from "@angular/material/button"
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
-    imports :  [MatButtonModule],
-    exports : [MatButtonModule]
+    imports :  [MatButtonModule,MatFormFieldModule,MatInputModule,MatIconModule ],
+    exports : [MatButtonModule,MatFormFieldModule,MatInputModule,MatIconModule ]
 })
 export class MaterialModule {
 
