d36c654d7d928af294e9552ac81ec6a07b5ab04c
trevin <60538411+trevin-livele@users.noreply.github.com>
2022-06-14 12:17:11 +0300

    added icons

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -9,7 +9,7 @@ import { MatNativeDateModule } from '@angular/material/core';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 import {MatSidenavModule} from '@angular/material/sidenav';
 import {MatToolbarModule} from '@angular/material/toolbar';
-
+import {MatListModule} from '@angular/material/list';
 
 
 @NgModule({
@@ -23,7 +23,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatNativeDateModule,
     MatCheckboxModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule
   ],
   exports: [
     MatButtonModule,
@@ -35,7 +36,8 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     MatNativeDateModule,
     MatCheckboxModule,
     MatSidenavModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatListModule
   ],
 })
 export class MaterialModule {}
