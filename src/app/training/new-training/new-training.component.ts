9c236a1e111f63869c4b6ecbc5a9660e06ac59d6
trevin <liveletrevin6@gmail.com>
2022-07-12 07:00:17 +0300

    connected to angular fire

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -3,6 +3,7 @@ import { Exercise } from '../exercise.model';
 import { TrainingService } from '../training.service';
 import { NgForm } from '@angular/forms';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-new-training',
@@ -10,15 +11,14 @@ import { AngularFirestore } from '@angular/fire/firestore';
   styleUrls: ['./new-training.component.css']
 })
 export class NewTrainingComponent implements OnInit {
- exercises: Exercise[] = []
+ exercises!: Observable<any>;
   constructor(private trainingService : TrainingService, private db: AngularFirestore) { }
 
   ngOnInit(): void {
-    this.db.collection('availableExercises').valueChanges().subscribe(result =>{
-      console.log(result);
-    });
+    this.exercises = this.db.collection('availableExercises').valueChanges();
+    };
 
-  }
+  
   onStartTraining(form: NgForm){
     this.trainingService.startExercise(form.value.exercise);
   }
