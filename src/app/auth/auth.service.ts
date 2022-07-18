e2f3079a400e675dad62962ad52cc913d3ffb9bf
trevin <liveletrevin6@gmail.com>
2022-07-18 12:00:04 +0300

    added spinner on signup and sign in

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -6,6 +6,7 @@ import { Router } from "@angular/router";
 import { AngularFireAuth } from '@angular/fire/auth';
 import { TrainingService } from "../training/training.service";
 import { MatSnackBar } from "@angular/material/snack-bar";
+import { UIService } from "../shared/ui.service";
 @Injectable()
 export class AuthService{
     authChange = new Subject<boolean>();
@@ -17,7 +18,8 @@ constructor(
     private router: Router,
      private afauth: AngularFireAuth,
      private trainingService : TrainingService,
-     private snackbar : MatSnackBar
+     private snackbar : MatSnackBar,
+     private uiService : UIService,
      ){}
 
 
@@ -37,11 +39,14 @@ initAuthListener(){
 }
 
     registerUser(authData: AuthData){
+        this.uiService.loadingStateChanged.next(true);
         this.afauth.auth.createUserWithEmailAndPassword(
             authData.email,
             authData.password
         ).then(result => {
+            this.uiService.loadingStateChanged.next(false);
         }).catch(error => {
+            this.uiService.loadingStateChanged.next(false);
             this.snackbar.open(error.message, null!, {
                 duration: 3000
             });
@@ -49,11 +54,14 @@ initAuthListener(){
     }
 
     login(authData: AuthData){
+    this.uiService.loadingStateChanged.next(true);
     this.afauth.auth
     .signInWithEmailAndPassword(authData.email,authData.password)
     .then(result => {
-   
+    this.uiService.loadingStateChanged.next(false);
+
     }).catch(error => {
+        this.uiService.loadingStateChanged.next(false);
         this.snackbar.open(error.message, null!, {
             duration: 3000
         });    });
--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { UIService } from 'src/app/shared/ui.service';
+import { Subscription } from 'rxjs';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -8,12 +9,17 @@ import { AuthService } from '../auth.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   loginForm!: FormGroup;
+  isLoading = false;
+  private loadingSubs!: Subscription;
 
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private uiService : UIService) {}
 
   ngOnInit() {
+    this.loadingSubs = this.uiService.loadingStateChanged.subscribe(isLoading => {
+      this.isLoading = isLoading;
+    })
     this.loginForm = new FormGroup({
       email: new FormControl('', {
         validators: [Validators.required, Validators.email]
@@ -28,4 +34,8 @@ export class LoginComponent implements OnInit {
       password: this.loginForm.value.password
     });
   }
+ngOnDestroy(): void {
+    this.loadingSubs.unsubscribe();
+}
+
 }
